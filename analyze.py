73c78ad39bc9c542e178089178f07e1b5fb8d99a
Simon Renblad <simon.renblad@outlook.com>
2021-08-24 20:32:16 +0800

    extract program pdfs from website

--- analyze.py
+++ analyze.py
@@ -1,6 +1,11 @@
 import pickle
+from igraph import *
 
 with open('courses.pickle', 'rb') as handle:
     courses = pickle.load(handle)
 
+#create a directed graph
 print(courses["IEDA3560"])
+
+g = Graph(directed=True)
+
---
+++ scrape_programs.py
@@ -0,0 +1,3 @@
+import pickle
+#https://prog-crs.ust.hk/ugprog/2021-22?token_post=42025789ea7b18be332277c1dd9c4721&is_s=Y&keyword=&school%5B%5D=SSCI&school%5B%5D=SENG&school%5B%5D=SBM&school%5B%5D=SHSS&school%5B%5D=IPO&career%5B%5D=req&career%5B%5D=major&career%5B%5D=BBA&career%5B%5D=BEng&career%5B%5D=BSc&career%5B%5D=EXTM-AI&career%5B%5D=dual&career%5B%5D=minor&year=2021-22&archive=
+# response.css('li > a[title]')
